5ebb502dc6363bc9a36ccfcb6d23a355d63b5335
joskr2 <josretamozovar@gmail.com>
2025-05-28 15:46:49 -0500

    estructura inicial

---
+++ script.js
@@ -0,0 +1,9 @@
+// ==========================================
+// 🚀 JAVASCRIPT + DOM 
+// ==========================================
+
+console.log("🎯 Iniciando clase de JavaScript + DOM");
+console.log(
+  "📝 Abre las herramientas de desarrollador para seguir los ejemplos"
+);
+
